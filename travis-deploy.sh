6621278f079fe8cf5d4d01a65340ee133d1cfbbf
Christoph Eickhoff <Christoph.Eickhoff@cs.uni-kassel.de>
2018-09-20 10:40:39 +0200

    deploy to mavencentral

---
+++ travis-deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+  openssl aes-256-cbc -K $encrypted_f70add2902af_key -iv $encrypted_f70add2902af_iv -in my.travis.gpg.enc -out my.travis.gpg -d
+  ./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=my.travis.gpg
+fi
