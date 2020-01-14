1d67363dc6a12c15f1027255e8bcc929edb94e50
Adrian Kunz <clashsoft@hotmail.com>
2020-01-14 09:15:35 +0100

    Deprecate fulib.od

--- src/main/java/fulib/od.java
+++ src/main/java/fulib/od.java
@@ -2,8 +2,17 @@ package fulib;
 
 import org.fulib.FulibTools;
 
+/**
+ * @deprecated since 1.2; use {@link FulibTools#objectDiagrams()} instead
+ */
+@Deprecated
 public class od
 {
+   /**
+    * @deprecated since 1.2; use {@link FulibTools#objectDiagrams()}.{@link
+    * org.fulib.tools.ObjectDiagrams#dumpPng(Object...) dumpPng(objects...)} instead
+    */
+   @Deprecated
    public static String dump(Object... objects)
    {
       return FulibTools.objectDiagrams().dumpPng(objects);
