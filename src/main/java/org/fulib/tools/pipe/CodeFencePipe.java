4db358c6ba4c5b34dce343dcae2881e235c5ba15
Adrian Kunz <clashsoft@hotmail.com>
2020-07-11 19:52:08 +0200

    Add JavaDoc to pipe classes

--- src/main/java/org/fulib/tools/pipe/CodeFencePipe.java
+++ src/main/java/org/fulib/tools/pipe/CodeFencePipe.java
@@ -1,5 +1,29 @@
 package org.fulib.tools.pipe;
 
+/**
+ * A pipe that puts the input text into a markdown code fence.
+ * E.g.,
+ *
+ * <pre>{@code
+ *    hello
+ *    world
+ * }</pre>
+ * <p>
+ * Is transformed to
+ *
+ * <pre>{@code
+ *    ```java
+ *    hello
+ *    world
+ *    ```
+ * }</pre>
+ * <p>
+ * You can specify a language tag either with the pipe argument (e.g. {@code fenced:js}).
+ * The default language tag is {@code java}, but you can override that using the {@link #CodeFencePipe(String)} constructor.
+ * The empty string is allowed as a language tag.
+ *
+ * @since 1.2
+ */
 public class CodeFencePipe implements Pipe
 {
    public static final String NAME = "fenced";
--- src/main/java/org/fulib/tools/pipe/JavaDocPipe.java
+++ src/main/java/org/fulib/tools/pipe/JavaDocPipe.java
@@ -1,5 +1,28 @@
 package org.fulib.tools.pipe;
 
+import org.fulib.tools.CodeFragments;
+
+/**
+ * A pipe that wraps the input text for use in Javadoc comments.
+ * E.g.,
+ * <pre>{@code
+ *    hello
+ *    world
+ * }</pre>
+ * <p>
+ * becomes
+ *
+ * <pre>{@code
+ *    <pre>{@code
+ *    hello
+ *    world
+ *    }</pre>
+ * }</pre>
+ * <p>
+ * Note that leading asterisks ({@code *}) are added by {@link CodeFragments}.
+ *
+ * @since 1.2
+ */
 public class JavaDocPipe implements Pipe
 {
    public static final String NAME = "javadoc";
