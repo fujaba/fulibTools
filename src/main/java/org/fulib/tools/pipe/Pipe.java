799163e31c5fa8ed3b5b0d87e2eab3d94cfc3dcc
Adrian Kunz <clashsoft@hotmail.com>
2020-07-11 20:26:24 +0200

    Make Pipe a FunctionalInterface

--- src/main/java/org/fulib/tools/pipe/Pipe.java
+++ src/main/java/org/fulib/tools/pipe/Pipe.java
@@ -2,11 +2,13 @@ package org.fulib.tools.pipe;
 
 /**
  * A pipe can transform the input text of a code fragment to a different output format.
+ * <p>
+ * This is a {@linkplain FunctionalInterface functional interface} and can be implemented using lambda expressions.
  *
  * @see org.fulib.tools.CodeFragments
- *
  * @since 1.2
  */
+@FunctionalInterface
 public interface Pipe
 {
    /**
