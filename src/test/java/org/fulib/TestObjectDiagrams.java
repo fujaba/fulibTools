ad230a1d3bf7c11edba63cdcc8109eb5881e24fb
Adrian Kunz <clashsoft@hotmail.com>
2020-08-13 18:25:06 +0200

    Use MatcherAssert in TestObjectDiagrams

--- src/test/java/org/fulib/TestObjectDiagrams.java
+++ src/test/java/org/fulib/TestObjectDiagrams.java
@@ -14,7 +14,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestObjectDiagrams
 {
