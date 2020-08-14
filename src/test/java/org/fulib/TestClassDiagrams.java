8a9e58006d6efb6f59cefec128955b4e903bcca8
Adrian Kunz <clashsoft@hotmail.com>
2020-08-14 12:14:46 +0200

    Chekk svg in TestClassDiagrams

--- src/test/java/org/fulib/TestClassDiagrams.java
+++ src/test/java/org/fulib/TestClassDiagrams.java
@@ -1,11 +1,13 @@
 package org.fulib;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.fulib.classmodel.ClassModel;
 import org.fulib.yaml.YamlIdMap;
 import org.junit.Test;
 import studyRight.StudyRight;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -13,7 +15,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestClassDiagrams
@@ -21,22 +23,33 @@ public class TestClassDiagrams
    @Test
    public void testClassDiagrams() throws IOException
    {
-      // load model
       final String packageName = StudyRight.class.getPackage().getName();
-
       try (final InputStream yamlInput = StudyRight.class.getResourceAsStream("classModel.yaml"))
       {
          final String yamlString = IOUtils.toString(yamlInput, StandardCharsets.UTF_8);
-         YamlIdMap idMap = new YamlIdMap(ClassModel.class.getPackage().getName());
-         ClassModel model = (ClassModel) idMap.decode(yamlString);
+         final YamlIdMap idMap = new YamlIdMap(ClassModel.class.getPackage().getName());
+         final ClassModel model = (ClassModel) idMap.decode(yamlString);
 
          model.setMainJavaDir("src/test/java");
 
          FulibTools.classDiagrams().dumpPng(model);
-
-         Path diagramPath = Paths.get("src/test/java/" + packageName.replace('.', '/') + "/doc-files/classDiagram.png");
-
-         assertThat(Files.exists(diagramPath), is(true));
+         FulibTools.classDiagrams().dumpPng(model, "tmp/classModel.png");
+         FulibTools.classDiagrams().dumpSVG(model, "tmp/classModel.svg");
       }
+
+      final File classDiagramPng = new File("src/test/java/" + packageName.replace('.', '/') + "/doc-files/classDiagram.png");
+      assertThat(classDiagramPng.exists(), equalTo(true));
+
+      assertThat(new File("tmp/classModel.png").exists(), equalTo(true));
+
+      final String svgText = FileUtils.readFileToString(new File("tmp/classModel.svg"), StandardCharsets.UTF_8);
+      assertThat(svgText, containsString("StudyRight"));
+      assertThat(svgText, containsString("id :String"));
+      assertThat(svgText, containsString("description :String"));
+      assertThat(svgText, containsString("uni"));
+      assertThat(svgText, containsString("students *"));
+      assertThat(svgText, containsString("Student"));
+      assertThat(svgText, containsString("name :String"));
+      assertThat(svgText, containsString("predicate :Predicate&lt;?&gt;"));
    }
 }
